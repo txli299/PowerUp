41cf953e6ac72398c9ca03651160a61ff713de4c
Cliff Li <txli299@gmail.com>
2023-09-20 14:18:43 -0500

    Initial Commit

---
+++ PowerUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PowerUp
+//
+//  Created by Cliff Li on 9/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        
+    }
+
+
+}
+
