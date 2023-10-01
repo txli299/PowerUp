03eb931e746f2a72fb562a8d6d3fb28c15cbbe26
Yucheng <hankgycc@gmail.com>
2023-10-01 16:26:12 -0400

    Wallet FrontPage-MVC

---
+++ PowerUp/WalletView/WalletView.swift
@@ -0,0 +1,20 @@
+//
+//  WalletView.swift
+//  PowerUp
+//
+//  Created by Hank Gu on 10/1/23.
+//
+
+import UIKit
+
+class WalletView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
