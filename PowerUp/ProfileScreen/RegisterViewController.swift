ca95f48c9b8d9be5bef1b76af1b0d5c39bb98e63
CliffLi <109610850+txli299@users.noreply.github.com>
2023-10-20 15:31:44 -0500

    Login, sign up functionality added (#21)

--- PowerUp/ProfileScreen/RegisterViewController.swift
+++ PowerUp/ProfileScreen/RegisterViewController.swift
@@ -1,17 +1,28 @@
 //
-//  ProfileScreenController.swift
+//  RegisterViewController.swift
 //  PowerUp
 //
-//  Created by Dingbang Chen on 10/2/23.
+//  Created by Cliff Li on 10/18/23.
 //
 
 import UIKit
 import FirebaseAuth
-class ProfileScreenController: UIViewController {
-    let profileScreen = ProfileScreenView()
+class RegisterViewController: UIViewController {
+    let registerView = RegisterView()
     
     override func loadView() {
-        view = profileScreen
+        view = registerView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerView.signupButton.addTarget(self, action: #selector(onButtonSignupTapped), for: .touchUpInside)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target:self , action: #selector(onButtonBackTapped))
+        
+    }
+    
+    @objc func onButtonBackTapped(){
+        let ProfileVC = ProfileScreenController()
+        navigationController?.pushViewController(ProfileVC, animated: true)
     }
 
     func alert(message:String){
@@ -34,7 +45,7 @@ class ProfileScreenController: UIViewController {
     func successMsg(){
         let alertController = UIAlertController(
                         title: "Success",
-                        message: "Sign in successfully",
+                        message: "Congradulation, you have created your account",
                         preferredStyle: .alert
                     )
         let okAction = UIAlertAction(
@@ -48,20 +59,10 @@ class ProfileScreenController: UIViewController {
         present(alertController, animated: true, completion: nil)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        profileScreen.loginButton.addTarget(self, action: #selector(onButtonSigninTapped), for: .touchUpInside)
-        profileScreen.registerButton.addTarget(self, action: #selector(onButtonRegisterTapped), for: .touchUpInside)
-    }
     
-    @objc func onButtonRegisterTapped(){
-        let registerVC = RegisterViewController()
-        navigationController?.pushViewController(registerVC, animated: true)
-    }
-    
-    @objc func onButtonSigninTapped(){
-        guard let email = profileScreen.userNameTextField.text, !email.isEmpty,
-              let password = profileScreen.passWordTextField.text, !password.isEmpty else{
+    @objc func onButtonSignupTapped(){
+        guard let email = registerView.userNameTextField.text, !email.isEmpty,
+              let password = registerView.passWordTextField.text, !password.isEmpty else{
             alert(message: "Please fill all fields")
             return
         }
@@ -70,16 +71,17 @@ class ProfileScreenController: UIViewController {
         //attemp sign in
         // fails then alert
         //
-        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { [weak self] result, error in
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { [weak self] result, error in
             guard let strongSelf = self else{
                 return
             }
             guard error == nil else{
-                strongSelf.alert(message: "Something wrong when signing in, please try again")
+                strongSelf.alert(message: "Something wrong when signing up, please try again")
                 return
             }
             strongSelf.successMsg()
-            
+            let profileVC = ProfileScreenController()
+            strongSelf.navigationController?.pushViewController(profileVC, animated: true)
         })
     }
 }
