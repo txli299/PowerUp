2c219dc2a869c43b7382a1b62cef51f94aa9a539
jcport1 <jefferson.cuartas@gmail.com>
2023-11-13 11:50:25 -0500

    Adds Stripe API stack view and pay function

--- PowerUp/WalletView/WalletViewController.swift
+++ PowerUp/WalletView/WalletViewController.swift
@@ -12,6 +12,8 @@ import StripePaymentsUI
 class WalletViewController: UIViewController {
     
     private let walletView = WalletView()
+    
+    var paymentIntentClientSecret: String?
 
     override func loadView() {
         view = walletView
@@ -20,10 +22,59 @@ class WalletViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         walletView.addFundsButton.addTarget(self, action: #selector(addFundsTapped), for: .touchUpInside)
+        walletView.stripePayButton.addTarget(self, action: #selector(pay), for: .touchUpInside)
+        startCheckout()
     }
     
+    // shows stripe card component
     @objc func addFundsTapped() {
-        print("Add Funds Button: I was clicked!")
-        // Redirect to Stripe's page to add funds
+        walletView.stripeStackView.isHidden = false
+    }
+    
+    func displayAlert(title: String){
+        
+    }
+    
+    @objc func pay() {
+        print("Stripe Button: I was clicked!")
+        
+        guard let paymentIntentClientSecret = paymentIntentClientSecret else {
+                    return
+                }
+                 //Collect card details
+                let paymentIntentParams = STPPaymentIntentParams(clientSecret: paymentIntentClientSecret)
+                paymentIntentParams.paymentMethodParams = walletView.stripeCardTextField.paymentMethodParams
+//                print(walletView.stripeCardTextField.paymentMethodParams.card)
+
+                // Submit the payment
+                let paymentHandler = STPPaymentHandler.shared()
+                paymentHandler.confirmPayment(paymentIntentParams, with: self) { (status, paymentIntent, error) in
+                    switch (status) {
+                    case .failed:
+                        self.displayAlert(title: "Payment failed")
+                        break
+                    case .canceled:
+                        self.displayAlert(title: "Payment canceled")
+                        break
+                    case .succeeded:
+                        self.displayAlert(title: "Payment succeeded")
+                        break
+                    @unknown default:
+                        fatalError()
+                        break
+                    }
+                }
+        }
+    
+    func startCheckout() {
+          // Request a PaymentIntent from your server and store its client secret
+          // Click View full sample to see a complete implementation
+      }
+    
+}
+
+extension WalletViewController: STPAuthenticationContext {
+    func authenticationPresentingViewController() -> UIViewController {
+        return self
     }
 }
